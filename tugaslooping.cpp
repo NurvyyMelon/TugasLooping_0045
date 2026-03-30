b6df0eb213e669d1921f385353ce717165b3a759
NurvyyMelon <adinnaufal9@gmail.com>
2026-03-30 12:26:11 +0700

    implementasi bool isprima

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -16,3 +16,12 @@ int main() {
 // ... (code sebelumnya)
 bool isPrima(int n);
 // ...
+bool isPrima(int n) {
+    if (n <= 1) return false;
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
